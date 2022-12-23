1ce84843fd2b8bcd0cc4777e58b7dc420a490fe5
Prashant <prashantpokhrel15@gmail.com>
2022-12-24 00:09:41 +1100

    about section completed

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./whatGPT3.css";
 const WhatGPT3 = () => {
-  return <div>What GPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature
+          title="What is GPT-3"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam accusantium rem possimus voluptatibus iure reiciendis soluta, nisi tempore ratione laudantium ex commodi doloremque. Cum mollitia illum doloribus. Error, corporis nobis placeat vitae laudantium tenetur neque deleniti in, quae animi dolore."
+        />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature
+          title="Chatbots"
+          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis architecto delectus quo repellendus inventore id!"
+        />
+        <Feature
+          title="Knowledgebase"
+          text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut nulla accusantium temporibus vitae, impedit necessitatibus dicta nam earum possimus animi."
+        />
+
+        <Feature
+          title="Education"
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste quos omnis minus magnam voluptatum, nisi libero unde eius, provident, illum facere."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
